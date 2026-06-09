449a2910d3fa7d6dc9ee6d92c0c2cc4aab2df881
Elyas Benyoub <56692302+ebenyoub@users.noreply.github.com>
2026-06-09 21:34:02 +0200

    Clean up Kasa project and setup GH actions (#1)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About'
 import Rentals from './pages/Rentals';
